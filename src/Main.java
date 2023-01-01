5bb21f4e2a1b19beeee12df6cce5fdfd8de58b4b
uguraycin <shishbecue@gmail.com>
2022-12-31 22:24:15 -0800

    second version

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("First version is complete");
+        System.out.println("version is complete");
     }
 }
